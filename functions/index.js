adc854b161281a9959f0d846336a25603fc0399f
Przemek Więch <pwiech@gmail.com>
2022-11-30 23:18:09 +0100

    Update settings for topola-viewer

--- functions/index.js
+++ functions/index.js
@@ -3,20 +3,19 @@ const corsAnywhere = require('cors-anywhere');
 const cors = require('cors');
 
 const corsServer = corsAnywhere.createServer({
-    originWhitelist: [
-      'http://localhost:3000',
-      'http://localhost:5000',
-      'https://portfolio-ravenous.web.app',
-      'https://portfolio-ravenous.firebaseapp.com'
-    ],
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
+  originWhitelist: [
+    'http://localhost:3000',
+    'https://pewu.github.io',
+    'https://apps.wikitree.com',
+  ],
+  requireHeader: ['origin', 'x-requested-with'],
+  removeHeaders: ['cookie', 'cookie2']
 });
 
 const corsHandler = cors({ origin: true });
 
 exports.proxy = onRequest((request, response) => {
-    corsHandler(request, response, () => {
-      corsServer.emit('request', request, response);
-    })
+  corsHandler(request, response, () => {
+    corsServer.emit('request', request, response);
+  })
 });
